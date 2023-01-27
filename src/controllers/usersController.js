22d43bbcd1154de3c5921f7c1ad3699c5e923758
rémi <dufour.remi59@gmail.com>
2023-01-27 23:10:55 +0100

    route users

---
+++ src/controllers/usersController.js
@@ -0,0 +1,56 @@
+export const addUser = (User) => {
+    return async (req,res) => {
+        try {
+            const user = new User({
+                username:req.body.username,
+                email:req.body.email,
+                password:req.body.password
+            })
+            const userSaved = await user.save();
+            res.status(201).json({success:true,data:userSaved});
+        } catch (err) {
+            res.status(400).json({success:false,error:err});
+        }
+    }  
+}
+
+export const updateUser = (User) => {
+    return async (req,res) => {
+        const user = {
+            username:req.body.username,
+            email:req.body.email,
+            password:req.body.password
+        }
+        User.findByIdAndUpdate(req.body.id,user,(err,userModified) => {
+            if(err){
+                res.status(400).json({success:false,error:err});
+            }else{
+                res.status(200).json({success:true});
+            }
+        })
+    }  
+} 
+
+export const findUser = (User) => {
+    return async (req,res) => {
+        User.findById(req.params.id, (err,user) => {
+            if(err){
+                res.status(400).json({success:false,data:err});
+            }else{
+                res.status(200).json({success:true,data:user});
+            }
+        });
+    }
+}
+
+export const deleteUser = (User) => {
+    return async (req,res) => {
+        User.findByIdAndRemove(req.params.id,(err,userRemoved) => {
+            if(err){
+                res.status(400).json({success:false,data:err});
+            }else{
+                res.status(200).json({success:true,data:userRemoved});
+            }
+        })
+    }
+}
--- src/router.js
+++ src/router.js
@@ -1,5 +1,11 @@
 import express from 'express'
 const router = express.Router()
+import {usersModel} from './models/usersModel.js'
+import { addUser,findUser,deleteUser,updateUser } from './controllers/usersController.js';
 
+router.route('/users/add').post(addUser(usersModel));
+router.route('/users/get/:id').get(findUser(usersModel));
+router.route('/users/remove/:id').delete(deleteUser(usersModel));
+router.route('/users/update').put(updateUser(usersModel));
 
 export default router;
